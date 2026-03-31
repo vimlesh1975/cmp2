dfb893a59b998801d6700fe98c25b3717bcb3917
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2026-03-31 13:13:28 +0530

    Refactor ucHS2 module logic

--- CasparMediaPlayback/ucHS2.vb
+++ CasparMediaPlayback/ucHS2.vb
@@ -1,157 +1,108 @@
-﻿Imports System.IO
+Imports System.IO
+
 Public Class ucHS2
+    Private Const HorizontalScrollDirectory As String = "c:\casparcg\mydata\HorizontalScroll2\"
+    Private Const FontsDirectory As String = "c:/casparcg/mydata/fonts"
+
     Private Sub cmdhidegbhs2_Click(sender As Object, e As EventArgs)
         Me.Hide()
     End Sub
+
     Private Sub cmbfonths2_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbfonths2.SelectedIndexChanged
-        'for 2nd
         On Error Resume Next
-        Dim fontname As String = cmbfonths2.Text
-        Dim fontsize As Integer = frmmediaplayer.nfontsizeforall.Value
-        Dim fs As New Font(fontname, fontsize, FontStyle.Regular)
-        Me.txtcrawl2.Font = fs
-        CasparCGDataCollection.Clear() 'cgData.Clear()
+
+        Dim fs As New Font(cmbfonths2.Text, frmmediaplayer.nfontsizeforall.Value, FontStyle.Regular)
+        txtcrawl2.Font = fs
+
         If chkbase64.Checked = False Then
-            CasparCGDataCollection.SetData("font", cmbfonths2.Text)
-            CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
+            UpdateCrawlData("font", cmbfonths2.Text)
         End If
     End Sub
+
     Private Sub cmdfile2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdfile2.Click
         On Error Resume Next
         ofd1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
-        ofd1.InitialDirectory = "c:\casparcg\mydata\HorizontalScroll2\"
-        If (ofd1.ShowDialog() = Windows.Forms.DialogResult.OK) Then
-            If (ofd1.FileName <> "") Then
-                Dim objFileInfo As FileInfo
-                objFileInfo = New FileInfo(ofd1.FileName)
+        ofd1.InitialDirectory = HorizontalScrollDirectory
 
-                ReadTextFile(objFileInfo.FullName, txtcrawl2)
-            End If
+        If ofd1.ShowDialog() = Windows.Forms.DialogResult.OK AndAlso ofd1.FileName <> "" Then
+            ReadTextFile(New FileInfo(ofd1.FileName).FullName, txtcrawl2)
         End If
     End Sub
+
     Private Sub cmdshowcrawl2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdshowcrawl2.Click
         On Error Resume Next
-        Dim a As Array
-        Dim str As String = ""
-        a = Split(txtcrawl2.Text, vbCrLf)
-        For ii = LBound(a) To UBound(a)
-            str = str + " ** " + a(ii)
-        Next
-        CasparCGDataCollection.Clear() 'cgData.Clear()
+        SetCrawlMainData()
 
-        If chkbase64.Checked Then
-            Dim array() As Byte = System.Text.Encoding.UTF8.GetBytes(str)
-            CasparCGDataCollection.SetData("base64", System.Convert.ToBase64String(array))
-
-        Else
-            CasparCGDataCollection.SetData("xf0", str)
-            CasparCGDataCollection.SetData("font", cmbfonths2.Text)
-        End If
-
-        CasparCGDataCollection.SetData("speed", Int(nspeed2.Value))
-        CasparCGDataCollection.SetData("size", nsize2.Value)
-
-
-        CasparCGDataCollection.SetData("y", ny2.Text)
-        CasparCGDataCollection.SetData("color", "0x" & String.Format("{0:X2}{1:X2}{2:X2}", cmdcolor2.ForeColor.R, cmdcolor2.ForeColor.G, cmdcolor2.ForeColor.B))
-        CasparCGDataCollection.SetData("stripcolor", "0x" & String.Format("{0:X2}{1:X2}{2:X2}", cmdcolor2.BackColor.R, cmdcolor2.BackColor.G, cmdcolor2.BackColor.B))
-
-        If chkclock2.Checked Then CasparCGDataCollection.SetData("alfa", 1) Else CasparCGDataCollection.SetData("alfa", 0)
         If chkltrhs2.Checked Then
-            CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Add(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), txths2TemplateLtoR.Text, True, CasparCGDataCollection.ToAMCPEscapedXml)
+            AddTemplateLayer(txths2TemplateLtoR.Text)
         Else
-            CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Add(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), txths2Template.Text, True, CasparCGDataCollection.ToAMCPEscapedXml)
+            AddTemplateLayer(txths2Template.Text)
         End If
     End Sub
+
     Private Sub cmdpause2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdpause2.Click
         On Error Resume Next
-        CasparCGDataCollection.Clear() 'cgData.Clear()
-        CasparCGDataCollection.SetData("speed", "0")
-        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
-
+        UpdateCrawlData("speed", "0")
     End Sub
+
     Private Sub cmdresume2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdresume2.Click
         On Error Resume Next
-        CasparCGDataCollection.Clear() 'cgData.Clear()
-        CasparCGDataCollection.SetData("speed", nspeed2.Value)
-        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
+        UpdateCrawlData("speed", nspeed2.Value)
     End Sub
+
     Private Sub cmdstopcrawl2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdstopcrawl2.Click
         On Error Resume Next
-        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Stop(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text))
+        StopTemplateLayer()
     End Sub
+
     Private Sub chkclock2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkclock2.CheckedChanged
         On Error Resume Next
-        CasparCGDataCollection.Clear() 'cgData.Clear()
-        If chkclock2.Checked Then CasparCGDataCollection.SetData("alfa", 1) Else CasparCGDataCollection.SetData("alfa", 0)
-        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
+        UpdateCrawlData("alfa", If(chkclock2.Checked, 1, 0))
     End Sub
 
     Private Sub cmdstripcolor2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdstripcolor2.Click
         On Error Resume Next
-        If (cd1.ShowDialog() = Windows.Forms.DialogResult.OK) Then
+        If cd1.ShowDialog() = Windows.Forms.DialogResult.OK Then
             cmdstripcolor2.BackColor = cd1.Color
             cmdcolor2.BackColor = cd1.Color
-            CasparCGDataCollection.Clear() 'cgData.Clear()
-            CasparCGDataCollection.SetData("stripcolor", "0x" & String.Format("{0:X2}{1:X2}{2:X2}", cmdcolor2.BackColor.R, cmdcolor2.BackColor.G, cmdcolor2.BackColor.B))
-
-            CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
+            UpdateCrawlData("stripcolor", ColorToCasparHex(cmdcolor2.BackColor))
         End If
     End Sub
+
     Private Sub nsize2_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nsize2.ValueChanged
         On Error Resume Next
-        CasparCGDataCollection.Clear()
-        CasparCGDataCollection.SetData("size", nsize2.Value)
-        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
+        UpdateCrawlData("size", nsize2.Value)
     End Sub
+
     Private Sub nspeed2_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nspeed2.ValueChanged
         On Error Resume Next
-        CasparCGDataCollection.Clear() 'cgData.Clear()
-        CasparCGDataCollection.SetData("speed", nspeed2.Value)
-        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
+        UpdateCrawlData("speed", nspeed2.Value)
     End Sub
+
     Private Sub txtcrawl2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtcrawl2.TextChanged
         On Error Resume Next
-        Dim a As Array
-        Dim str As String = ""
-        a = Split(txtcrawl2.Text, vbCrLf)
-        For ii = LBound(a) To UBound(a)
-            str = str + " " + a(ii)
-        Next
-        CasparCGDataCollection.Clear() 'cgData.Clear()
-
-        If chkbase64.Checked Then
-            Dim array() As Byte = System.Text.Encoding.UTF8.GetBytes(str)
-            CasparCGDataCollection.SetData("base64", System.Convert.ToBase64String(array))
-        Else
-            CasparCGDataCollection.SetData("xf0", str)
-            CasparCGDataCollection.SetData("font", cmbfonths2.Text)
-        End If
-        CasparDevice.Channels(Int(g_int_ChannelNumber) - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
+        SetCrawlTextData(False)
+        UpdateTemplateLayer()
     End Sub
+
     Private Sub cmdcolor2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdcolor2.Click
         On Error Resume Next
-        If (cd1.ShowDialog() = Windows.Forms.DialogResult.OK) Then
+        If cd1.ShowDialog() = Windows.Forms.DialogResult.OK Then
             cmdcolor2.ForeColor = cd1.Color
             cmdstripcolor2.ForeColor = cd1.Color
-
-            CasparCGDataCollection.Clear() 'cgData.Clear()
-            CasparCGDataCollection.SetData("color", "0x" & String.Format("{0:X2}{1:X2}{2:X2}", cmdcolor2.ForeColor.R, cmdcolor2.ForeColor.G, cmdcolor2.ForeColor.B))
-            CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
+            UpdateCrawlData("color", ColorToCasparHex(cmdcolor2.ForeColor))
         End If
     End Sub
 
     Private Sub ny2_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ny2.ValueChanged
         On Error Resume Next
-        CasparCGDataCollection.Clear() 'cgData.Clear()
-        CasparCGDataCollection.SetData("y", ny2.Value)
-        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(Int(cmblayerhs2.Text), Int(cmblayerhs2.Text), CasparCGDataCollection)
+        UpdateCrawlData("y", ny2.Value)
     End Sub
+
     Sub enumfonts()
         On Error Resume Next
-        For Each Filefound As String In Directory.GetFiles("c:/casparcg/mydata/fonts", "*.swf")
-            Dim stringtocut As Integer = 26
-            Dim filefound1 = Split(Mid(Filefound, stringtocut), ".")
+        For Each fileFound As String In Directory.GetFiles(FontsDirectory, "*.swf")
+            Dim filefound1 = Split(Mid(fileFound, 26), ".")
             cmbfonths2.Items.Add(filefound1(0))
         Next
     End Sub
@@ -159,4 +110,72 @@ Public Class ucHS2
     Private Sub ucHS2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         enumfonts()
     End Sub
+
+    Private Function GetLayerNumber() As Integer
+        Return Int(cmblayerhs2.Text)
+    End Function
+
+    Private Sub ClearCrawlData()
+        CasparCGDataCollection.Clear()
+    End Sub
+
+    Private Sub AddTemplateLayer(templatePath As String)
+        Dim layerNumber As Integer = GetLayerNumber()
+        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Add(layerNumber, layerNumber, templatePath, True, CasparCGDataCollection.ToAMCPEscapedXml)
+    End Sub
+
+    Private Sub UpdateTemplateLayer()
+        Dim layerNumber As Integer = GetLayerNumber()
+        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Update(layerNumber, layerNumber, CasparCGDataCollection)
+    End Sub
+
+    Private Sub StopTemplateLayer()
+        Dim layerNumber As Integer = GetLayerNumber()
+        CasparDevice.Channels(g_int_ChannelNumber - 1).CG.Stop(layerNumber, layerNumber)
+    End Sub
+
+    Private Sub UpdateCrawlData(key As String, value As Object)
+        ClearCrawlData()
+        CasparCGDataCollection.SetData(key, value)
+        UpdateTemplateLayer()
+    End Sub
+
+    Private Sub SetCrawlMainData()
+        ClearCrawlData()
+        SetCrawlTextData(True)
+        CasparCGDataCollection.SetData("speed", Int(nspeed2.Value))
+        CasparCGDataCollection.SetData("size", nsize2.Value)
+        CasparCGDataCollection.SetData("y", ny2.Text)
+        CasparCGDataCollection.SetData("color", ColorToCasparHex(cmdcolor2.ForeColor))
+        CasparCGDataCollection.SetData("stripcolor", ColorToCasparHex(cmdcolor2.BackColor))
+        CasparCGDataCollection.SetData("alfa", If(chkclock2.Checked, 1, 0))
+    End Sub
+
+    Private Sub SetCrawlTextData(useDoubleSeparator As Boolean)
+        Dim crawlText As String = BuildCrawlText(useDoubleSeparator)
+
+        If chkbase64.Checked Then
+            Dim array() As Byte = System.Text.Encoding.UTF8.GetBytes(crawlText)
+            CasparCGDataCollection.SetData("base64", System.Convert.ToBase64String(array))
+        Else
+            CasparCGDataCollection.SetData("xf0", crawlText)
+            CasparCGDataCollection.SetData("font", cmbfonths2.Text)
+        End If
+    End Sub
+
+    Private Function BuildCrawlText(useDoubleSeparator As Boolean) As String
+        Dim lines As Array = Split(txtcrawl2.Text, vbCrLf)
+        Dim separator As String = If(useDoubleSeparator, " ** ", " ")
+        Dim result As String = ""
+
+        For ii = LBound(lines) To UBound(lines)
+            result &= separator & lines(ii)
+        Next
+
+        Return result
+    End Function
+
+    Private Function ColorToCasparHex(colorValue As Color) As String
+        Return "0x" & String.Format("{0:X2}{1:X2}{2:X2}", colorValue.R, colorValue.G, colorValue.B)
+    End Function
 End Class
--- REFACTOR_TRACKER.md
+++ REFACTOR_TRACKER.md
@@ -25,6 +25,7 @@ This file tracks module-by-module cleanup/refactor work so we can keep momentum 
 | `UcCaption` | `CasparMediaPlayback/UcCaption.vb` | Done | Caption layer-address, fill updates, and resize synchronization centralized into small helpers. |
 | `ucElement` | `CasparMediaPlayback/ucElement.vb` | Done | Media-source setup, layer command building, preview playback, fill updates, and YouTube/file helpers consolidated. |
 | `ucHS` | `CasparMediaPlayback/ucHS.vb` | Done | Horizontal-scroll layer helpers, logo open/play/stop logic, color updates, and crawl data building consolidated. |
+| `ucHS2` | `CasparMediaPlayback/ucHS2.vb` | Done | Secondary horizontal-scroll layer helpers, crawl text/data building, and color/font updates consolidated. |
 
 ## Repository Cleanup
 
